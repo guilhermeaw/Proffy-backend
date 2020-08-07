1883d755de3a54582db0e1d32802789fc6764a49
guilhermeaw <guilherme.w@universo.univates.br>
2020-08-06 22:56:21 -0300

    Added database config

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+app.post('/', (request, response) => {
+  return response.json({
+    message: 'Hello'
+  });
+});
+
+app.listen(3333, () => {
+  console.log('running');
+});
