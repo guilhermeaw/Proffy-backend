4410c405f11cfe45ffa98f1aa7c92c8994199bb7
guilhermeaw <guilherme.w@universo.univates.br>
2020-08-07 23:58:08 -0300

    Created controllers

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,16 @@
 import express from 'express';
+import ClassesController from './controllers/ClassesController';
+import ConnectionsController from './controllers/ConnectionsController';
 
 const routes = express.Router();
 
-routes.get('/', (request, response) => {
-  return response.json({message: 'Hello'});
-});
+const classesController = new ClassesController();
+const connectionsController = new ConnectionsController();
+
+routes.post('/classes', classesController.create);
+routes.get('/classes', classesController.index);
+
+routes.post('/connections', connectionsController.create);
+routes.get('/connections', connectionsController.index);
 
 export default routes;
